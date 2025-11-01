cd824c6f8f60bee8193a71d6490ee8228b44fab1
dekitate-com <wonderfulkiyo@gmail.com>
2025-11-01 19:22:07 +0900

    feat: single page

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,9 +1,18 @@
-import { createFileRoute } from "@tanstack/react-router";
+import { createFileRoute, Link } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/")({
 	component: Index,
 });
 
 function Index() {
-	return <h2>Welcome Home!</h2>;
+	return (
+		<div>
+			<h2>Welcome Home!</h2>
+			<ul>
+				<li>
+					<Link to="/about">About</Link>
+				</li>
+			</ul>
+		</div>
+	);
 }
