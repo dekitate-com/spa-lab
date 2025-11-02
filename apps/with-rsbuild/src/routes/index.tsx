e18a86ed43d5295ea9f0a23f3a857cd0d02bc3bc
dekitate-com <wonderfulkiyo@gmail.com>
2025-11-02 14:29:12 +0900

    feat: CSS Modules のサンプルを追加 (#9)

--- apps/with-rsbuild/src/routes/index.tsx
+++ apps/with-rsbuild/src/routes/index.tsx
@@ -1,9 +1,16 @@
 import { createFileRoute } from "@tanstack/react-router";
+import { Button } from "~/components/button";
 
 export const Route = createFileRoute("/")({
 	component: Index,
 });
 
 function Index() {
-	return <h2>Hello Rsbuild!</h2>;
+	return (
+		<>
+			<h2>Hello Rsbuild!</h2>
+			<Button>Primary Button</Button>
+			<Button variant="secondary">Secondary Button</Button>
+		</>
+	);
 }
